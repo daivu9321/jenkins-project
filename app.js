e7d890bd150cb9e7886e28820d3ac34b11feeb5b
daivu9321 <daibom99937271@gmail.com>
2022-09-13 13:31:56 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('codestar devops!!!'))
+app.listen(3000, () => console.log('Check container ecs!!!'))
