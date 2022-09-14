68d0bcf78578224d35c1f0b32779a3188590288c
daivu9321 <daibom99937271@gmail.com>
2022-09-15 03:32:07 +0700

    dsad

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('codestar devops!!!'))
+app.get('/', (req, res) => res.send('code devops!!!'))
 app.listen(3000, () => console.log('Check container ecs!!!'))
